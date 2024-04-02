4c33241fcabc7300b4798a1564fb212db6e635b2
Nitesh Kumar <niteshkumaro7@hotmail.com>
2024-04-02 18:25:09 +0530

    NodeJs-Realtime-Chat-App

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    try {
+        const r = await axios.put(
+          "https://api.chatengine.io/users/",
+          { username: username, secret: username, first_name: username },
+          { headers: { "private-key": "Your-Private-Key" } }  // Replace with your Chat Engine private key  
+        );
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+       if (e.response) {
+         return res.status(e.response.status).json(e.response.data);
+       } else {
+         console.error(e);
+         return res.status(500).json({ error: "An error occurred" });
+       }
+    }
+});
+
+app.listen(3001);
