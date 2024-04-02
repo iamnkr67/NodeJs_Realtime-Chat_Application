27f9ab12f8feb297fc0f120c7134f48a0ea609d5
Nitesh Kumar <niteshkumaro7@hotmail.com>
2024-04-02 22:19:32 +0530

    Changes

--- Backend/index.js
+++ Backend/index.js
@@ -7,22 +7,22 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-    const { username } = req.body;
-    try {
-        const r = await axios.put(
-          "https://api.chatengine.io/users/",
-          { username: username, secret: username, first_name: username },
-          { headers: { "private-key": "Your-Private-Key" } }  // Replace with your Chat Engine private key  
-        );
-        return res.status(r.status).json(r.data)
-    } catch (e) {
-       if (e.response) {
-         return res.status(e.response.status).json(e.response.data);
-       } else {
-         console.error(e);
-         return res.status(500).json({ error: "An error occurred" });
-       }
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "b1d6faa5-ea41-4b87-b8e8-7501ecc7ab0f" } } // Replace with your Chat Engine private key
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    if (e.response) {
+      return res.status(e.response.status).json(e.response.data);
+    } else {
+      console.error(e);
+      return res.status(500).json({ error: "An error occurred" });
     }
+  }
 });
 
 app.listen(3001);
